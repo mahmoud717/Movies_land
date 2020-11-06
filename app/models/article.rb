95136bbed38963393a3bcd8b2fbacb9c6c4a82e9
mahmoud717 <mahmoudmohammad717@gmail.com>
2020-11-06 15:23:23 +0200

    added comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ApplicationRecord
     has_many :categories, through: :tags 
     belongs_to :user, foreign_key: "author_id"
     has_many :votes
+    has_many :comments, foreign_key: "article_id"
 end
