02fbcaf4bba61fa844ccaa96d7ae764eb131c74e
mahmoud717 <mahmoudmohammad717@gmail.com>
2020-11-07 14:31:24 +0200

    installed bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_094216) do
+ActiveRecord::Schema.define(version: 2020_11_06_125113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2020_11_06_094216) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "priority"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "article_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
